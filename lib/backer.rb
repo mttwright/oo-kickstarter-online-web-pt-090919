3d7fe20c1248a24fbc9d6c19b26d768fa8a436ee
virtual-frame-7789 <mttwright@gmail.com>
2019-10-13 04:43:52 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,9 @@
 class Backer
   
-  attr_reader :backed_projects
+  attr_reader :name, :backed_projects
   
-  def initialize
+  def initialize(name)
+    @name = name
     @backed_projects = []
   end
   
